9db4cb106c703b6e41906e8e26bb8c160e0aa899
Chazona Baum <chazona@chazonabaum.com>
2018-10-11 22:16:29 -0400

    Initial commit

---
+++ functions.php
@@ -0,0 +1,6 @@
+<?php
+
+/*
+* Add your own functions here. You can also copy some of the theme functions into this file. 
+* Wordpress will use those functions instead of the original functions then.
+*/
